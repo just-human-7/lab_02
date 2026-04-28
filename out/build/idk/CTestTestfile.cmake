1fe830e9ed1c96b0cc53d7fc7bd3e9ad4bfb7e5b
just-human-7 <chelovek9977@gmail.com>
2026-04-28 07:24:37 +0700

    Delete out/build/idk directory

--- out/build/idk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/vsc_projects/lab_02
-# Build directory: D:/vsc_projects/lab_02/out/build/idk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
